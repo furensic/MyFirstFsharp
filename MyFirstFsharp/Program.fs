be1f996d5be39651a0cc39d5e33cb8bf1e0817dd
Leo <info@furensic.eu>
2025-05-12 00:02:47 +0200

    Hello World, basic list operations

---
+++ MyFirstFsharp/Program.fs
@@ -0,0 +1,12 @@
+﻿printfn "Hello World from F#"
+
+let square x = x * x
+
+let num1 = square 2
+printfn "square: %A" num1
+
+[0..100] |> List.map square |> printfn "%A"
+
+let squareToN n = [0..n] |> List.map square
+
+squareToN 5 |> printfn "square: %A"
